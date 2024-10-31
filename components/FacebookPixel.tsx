720da707376becb3c048998f197652d5f83dc11a
Lemons <68083649+lemonsagency@users.noreply.github.com>
2024-10-31 12:11:07 -0300

    tet

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -4,7 +4,6 @@ import { usePathname, useSearchParams } from 'next/navigation'
 import Script from 'next/script'
 import { useEffect } from 'react'
 import Image from 'next/image'
-import type { FacebookPixelParams } from '../types/facebook-pixel'
 
 export default function FacebookPixel({ pixelId }: { pixelId: string }) {
   const pathname = usePathname()
