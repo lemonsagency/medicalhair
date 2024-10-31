8c29bac1e33600ec0f3301d981f315d693260b27
Lemons <68083649+lemonsagency@users.noreply.github.com>
2024-10-31 15:15:19 -0300

    test

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -2,52 +2,23 @@
 
 import { useEffect } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
-
-declare global {
-  interface Window {
-    fbq: (...args: unknown[]) => void;
-    _fbq: unknown;
-  }
-}
+import ReactPixel from 'react-facebook-pixel'
 
 export default function FacebookPixel({ pixelId }: { pixelId: string }) {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
   useEffect(() => {
-    // Initialize Facebook Pixel
-    const initFbq = () => {
-      const fbq = function(...args: unknown[]) {
-        if (fbq.callMethod) {
-          fbq.callMethod.apply(fbq, args);
-        } else {
-          fbq.queue.push(args);
-        }
-      };
-      fbq.push = fbq;
-      fbq.loaded = true;
-      fbq.version = '2.0';
-      fbq.queue = [];
-      window.fbq = fbq;
-    };
+    ReactPixel.init(pixelId)
+    ReactPixel.pageView()
 
-    if (!window.fbq) {
-      initFbq();
-      const script = document.createElement('script');
-      script.async = true;
-      script.src = 'https://connect.facebook.net/en_US/fbevents.js';
-      document.head.appendChild(script);
-    }
-
-    window.fbq('init', pixelId);
-    window.fbq('track', 'PageView');
-
-    // Track page views on route changes
+    // This function will run on route changes
     const handleRouteChange = () => {
-      window.fbq('track', 'PageView');
+      ReactPixel.pageView()
     }
 
-    handleRouteChange();
+    // Call it once on mount
+    handleRouteChange()
   }, [pixelId, pathname, searchParams])
 
   return null
